bc4953fbc74e1e35f71fbec4cb7d800b65d1a9fe
skoosm990 <161984156+skoosm990@users.noreply.github.com>
2025-03-08 17:33:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/currency-converter/',
+  base: '/currency-converter-2/',
   test: {
     globals: true,
     environment: 'jsdom',
